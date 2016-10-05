fab9344a1e923e5d23bf85275657732291965a57
Angel Del Castillo <jubu201504@gmail.com>
2016-10-05 02:57:57 -0500

    change for NewSoapRequest and NewRawRequest

--- wsdl.go
+++ wsdl.go
@@ -24,36 +24,55 @@
 package wsdl
 
 import (
+	"bytes"
 	"encoding/xml"
+	"errors"
 	"io"
 	"net/http"
-	"time"
 )
 
-// WSDL struct defines _
+var (
+	// ErrClientNil error is returned when SetClient method
+	// fails.
+	ErrClientNil = errors.New("wsdl: http client is nil")
+)
+
+// WSDL client.
 type WSDL struct {
 	client *http.Client
 }
 
 // New returns a new WSDL client.
-func New(timeout time.Duration) (*WSDL, error) {
-	c := &WSDL{
-		client: &http.Client{
-			Timeout: timeout,
-		},
+func New(client *http.Client) (*WSDL, error) {
+	if client == nil {
+		return nil, ErrClientNil
+	}
+	wl := &WSDL{
+		client: client,
 	}
 
-	return c, nil
+	return wl, nil
 }
 
-// Do makes a request.
-func (c *WSDL) Do(body io.Reader, url, action string, dst io.Writer) error {
-	req, err := http.NewRequest("POST", url, body)
+// Custom method is keep for custom requests.
+func (c *WSDL) Custom(r *http.Request, dst interface{}) error {
+	resp, err := c.client.Do(r)
 	if err != nil {
 		return err
 	}
-	req.Header.Add("Content-Type", "text/xml; charset=utf-8")
-	req.Header.Add("SOAPAction", action)
+	defer resp.Body.Close()
+
+	err = xml.NewDecoder(resp.Body).Decode(dst)
+	return err
+
+}
+
+// Soap make a SOAP call to endpoint.
+func (c *WSDL) Soap(src, dst interface{}, url, action string) error {
+	req, err := NewSoapRequest(src, url, action)
+	if err != nil {
+		return err
+	}
 
 	resp, err := c.client.Do(req)
 	if err != nil {
@@ -61,12 +80,35 @@ func (c *WSDL) Do(body io.Reader, url, action string, dst io.Writer) error {
 	}
 	defer resp.Body.Close()
 
-	_, err = io.Copy(dst, resp.Body)
+	err = xml.NewDecoder(resp.Body).Decode(dst)
 	return err
 }
 
-// Soap make a SOAP call to endpoint.
-func (c *WSDL) Soap(body, response xml.Name) error {
-	// TODO;
-	return nil
+// NewSoapRequest returns a prepared SOAP request.
+func NewSoapRequest(src interface{}, url, action string) (*http.Request, error) {
+	// FIXME; make WSDL buffer?
+	buf := bytes.NewBuffer([]byte{})
+	err := xml.NewEncoder(buf).Encode(src)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", url, buf)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "text/xml;charset=UTF-8")
+	req.Header.Set("SOAPAction", action)
+	return req, nil
+}
+
+// NewRawRequest returns a prepared SOAP request.
+func NewRawRequest(body io.Reader, url, action string) (*http.Request, error) {
+	req, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "text/xml;charset=UTF-8")
+	req.Header.Set("SOAPAction", action)
+	return req, nil
 }
--- wsdl_test.go
+++ wsdl_test.go
@@ -24,14 +24,222 @@
 package wsdl
 
 import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 )
 
-func TestNew(t *testing.T) {
-	// TODO; make test
-	_, err := New(time.Second)
+type T struct {
+	Purpose       string
+	Input         Input
+	ExpectedError error
+}
+
+type Input struct {
+	Client   *http.Client
+	Endpoint string
+	Action   string
+	Username string
+	Password string
+}
+
+func TestSoap(t *testing.T) {
+	tsBasic := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, tmplResponse)
+	}))
+	defer tsBasic.Close()
+
+	table := []T{
+		T{
+			Input: Input{
+				Client: &http.Client{
+					Timeout: time.Second,
+				},
+				Endpoint: tsBasic.URL,
+				Action:   "",
+			},
+			ExpectedError: nil,
+		},
+	}
+
+	for i := range table {
+		x := table[i]
+
+		wclient, err := New(x.Input.Client)
+		if err != nil {
+			t.Errorf("err : [%s]", err)
+		}
+
+		src := &SampleRequest{
+			XSI: "Something",
+		}
+
+		var res *SampleResponse
+		err = wclient.Soap(src, &res, x.Input.Endpoint, x.Input.Action)
+		if err != x.ExpectedError {
+			t.Errorf("expected [%s] actual [%s]", x.ExpectedError, err)
+		}
+
+		log.Printf("soap res [%#v]", res)
+	}
+}
+
+// TestCustom demonstrates a custom request (basic auth, middlewares, etc.)
+func TestCustom(t *testing.T) {
+	tsBasic := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		u, p, _ := r.BasicAuth()
+		if u != "admin" && p != "123456" {
+			t.Logf("basic auth header not found")
+			fmt.Fprintln(w, "unauthorized")
+			return
+		}
+
+		fmt.Fprintln(w, tmplResponse)
+	}))
+	defer tsBasic.Close()
+
+	table := []T{
+		T{
+			Purpose: "Custom method with basic auth",
+			Input: Input{
+				Client: &http.Client{
+					Timeout: time.Second,
+				},
+				Endpoint: tsBasic.URL,
+				Action:   "",
+				Username: "admin",
+				Password: "123456",
+			},
+			ExpectedError: nil,
+		},
+		T{
+			Purpose: "Custom method with Failing auth",
+			Input: Input{
+				Client: &http.Client{
+					Timeout: time.Second,
+				},
+				Endpoint: tsBasic.URL,
+				Action:   "",
+				Username: "",
+				Password: "",
+			},
+			ExpectedError: io.EOF,
+		},
+	}
+
+	for i := range table {
+		x := table[i]
+
+		wclient, err := New(x.Input.Client)
+		if err != nil {
+			t.Errorf("err : [%s]", err)
+		}
+
+		src := &SampleRequest{
+			Envelope: "xx",
+		}
+
+		req, err := NewSoapRequest(src, x.Input.Endpoint, x.Input.Action)
+		if err != nil {
+			t.Errorf("err : [%s]", err)
+		}
+		req.SetBasicAuth(x.Input.Username, x.Input.Password)
+
+		var res *SampleResponse
+		err = wclient.Custom(req, &res)
+		if err != x.ExpectedError {
+			t.Errorf("expected [%s] actual [%s]", x.ExpectedError, err)
+		}
+
+		log.Printf("soap res [%#v]", res)
+	}
+}
+
+func TestRaw(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		u, p, _ := r.BasicAuth()
+		if u != "admin" && p != "123456" {
+			t.Logf("basic auth header not found")
+			fmt.Fprintln(w, "unauthorized")
+			return
+		}
+
+		fmt.Fprintln(w, tmplResponse)
+	}))
+	defer ts.Close()
+
+	wclient, err := New(http.DefaultClient)
 	if err != nil {
 		t.Errorf("err : [%s]", err)
 	}
+
+	b := []byte(tmplRequest)
+	buf := bytes.NewBuffer(b)
+
+	req, err := NewRawRequest(buf, ts.URL, "")
+	if err != nil {
+		t.Errorf("err : [%s]", err)
+	}
+	req.SetBasicAuth("admin", "123456")
+
+	var res *SampleResponse
+	err = wclient.Custom(req, &res)
+	if err != nil {
+		t.Errorf("err [%s]", err)
+	}
+
+	log.Printf("soap res [%#v]", res)
+
 }
+
+type SampleRequest struct {
+	XMLName  xml.Name `xml:"http://schemas.xmlsoap.org/soap/envelope/ Envelope"`
+	Envelope string   `xml:"SOAP-ENV,attr"`
+	XSI      string   `xml:"xmlns xsi,attr"`
+	XSD      string   `xml:"xmlns xsd,attr"`
+	Soap     string   `xml:"xmlns soap,attr"`
+}
+
+type SampleResponse struct {
+	XMLName  xml.Name `xml:"http://schemas.xmlsoap.org/soap/envelope/ Envelope"`
+	Envelope string   `xml:"SOAP-ENV,attr"`
+	XSI      string   `xml:"xmlns xsi,attr"`
+	XSD      string   `xml:"xmlns xsd,attr"`
+	Soap     string   `xml:"xmlns soap,attr"`
+	Body     ResBody
+}
+
+type ResBody struct {
+	XMLName                  string `xml:"http://namespaces.snowboard-info.com m"`
+	EndorsingBoarderResponse string `xml:"GetEndorsingBoarderResponse,attr"`
+	Endorsing                string `xml:"endorsingBoarder"`
+}
+
+const (
+	tmplRequest = `<SOAP-ENV:Envelope
+  xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
+  SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+  <SOAP-ENV:Body>
+    <m:GetEndorsingBoarder xmlns:m="http://namespaces.snowboard-info.com">
+      <manufacturer>K2</manufacturer>
+      <model>Fatbob</model>
+    </m:GetEndorsingBoarder>
+  </SOAP-ENV:Body>
+</SOAP-ENV:Envelope>`
+
+	tmplResponse = `<SOAP-ENV:Envelope
+  xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
+  SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+  <SOAP-ENV:Body>
+    <m:GetEndorsingBoarderResponse xmlns:m="http://namespaces.snowboard-info.com">
+      <endorsingBoarder>Chris Englesmann</endorsingBoarder>
+    </m:GetEndorsingBoarderResponse>
+  </SOAP-ENV:Body>
+</SOAP-ENV:Envelope>`
+)
